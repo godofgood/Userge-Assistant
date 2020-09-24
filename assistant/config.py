730e113bf339124e81f26fbf16be1438ad6801af
godofgood <64130518+godofgood@users.noreply.github.com>
2020-09-24 18:46:42 +0530

    Update config.py

--- assistant/config.py
+++ assistant/config.py
@@ -18,20 +18,18 @@ if os.path.isfile("config.env"):
 
 class Config:
     """ assistant configs """
-    APP_ID = int(os.environ.get("APP_ID", 0))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    AUTH_CHATS = set([-1001481357570])  # @UserGeOt
+    APP_ID = int(os.environ.get("1511336", 0))
+    API_HASH = os.environ.get("8f670902d751008396e20f91505892b6")
+    BOT_TOKEN = os.environ.get("1272994884:AAECqOlERs6M0RVwbgBIs4A2h5yX03zkACY")
+    AUTH_CHATS = set([-1001169690724])  # @Hacking_burners
     if os.environ.get("AUTH_CHATS"):
         AUTH_CHATS.update(map(int, os.environ.get("AUTH_CHATS").split()))
-    WHITELIST_CHATS = set([-1001465749479])  # @UserGeSpam
+    WHITELIST_CHATS = set([-1001169690724])  # @Hacking_burners
     if os.environ.get("WHITELIST_CHATS"):
         WHITELIST_CHATS.update(map(int, os.environ.get("WHITELIST_CHATS").split()))
     DEV_USERS = (
-        1158855661,  # @Krishna_Singhal
-        1110621941,  # @PhycoNinja13b
-        921420874,   # @juznem
-        837784353    # @rking_32
+        1013793759,  # @Legitxd
+        
     )
     ADMINS = {}
     MAX_MSG_LENGTH = 4096
